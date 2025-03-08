4753b5fe09db8facf0e52ac7422959833b8c8f4c
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2025-03-08 21:04:52 +0000

    Update page.tsx

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -1,15 +1,5 @@
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
-
-import { CTA } from '@/templates/CTA';
-import { DemoBanner } from '@/templates/DemoBanner';
-import { FAQ } from '@/templates/FAQ';
-import { Features } from '@/templates/Features';
-import { Footer } from '@/templates/Footer';
-import { Hero } from '@/templates/Hero';
-import { Navbar } from '@/templates/Navbar';
-import { Pricing } from '@/templates/Pricing';
-import { Sponsors } from '@/templates/Sponsors';
-
+import LandingPageV2 from '@/templates/landingv2';
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
     locale: props.params.locale,
@@ -27,17 +17,11 @@ const IndexPage = (props: { params: { locale: string } }) => {
 
   return (
     <>
-      <DemoBanner />
-      <Navbar />
-      <Hero />
-      <Sponsors />
-      <Features />
-      <Pricing />
-      <FAQ />
-      <CTA />
-      <Footer />
+      <LandingPageV2 />
     </>
   );
 };
+
+
 
 export default IndexPage;
