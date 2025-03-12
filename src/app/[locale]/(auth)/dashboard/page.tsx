8aef14976fdeb4d2198dddc755dc7d2ddd248212
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2025-03-12 17:37:02 +0100

    Update page.tsx

--- src/app/[locale]/(auth)/dashboard/page.tsx
+++ src/app/[locale]/(auth)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { auth } from '@clerk/nextjs/server'
 import { useTranslations } from 'next-intl';
 
 import DashboardPage from '@/components/dashboard';
@@ -6,8 +5,7 @@ import { TitleBar } from '@/features/dashboard/TitleBar';
 
 const DashboardIndexPage = async () => {
   const t = useTranslations('DashboardIndex');
-  const { userId, redirectToSignIn } = await auth()
-  if (!userId) return redirectToSignIn()
+
 
   return (
     <>
@@ -15,16 +13,7 @@ const DashboardIndexPage = async () => {
           title={t('title_bar')}
           description={t('title_bar_description')}
         />
-     
-      {userId
-        ? (
-            <DashboardPage sessionId={"sessionId"} userId={userId} />
-          )
-        : (
-            <div className="p-4 text-center">
-              {t('authentication_required')}
-            </div>
-          )}
+        <DashboardPage sessionId={"sessionId"} userId={""} />  
     </>
   );
 };
